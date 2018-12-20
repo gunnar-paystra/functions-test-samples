7f72f22592651e5fbe1ab02c174bdb12fb41ef25
Gunnar <gunnar@paystra.com>
2018-12-20 04:30:46 -0800

    add test files

---
+++ CSharpOddOrEven/CSharpOddOrEven/OddOrEvenQueue.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net.Http;
+using System.Numerics;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace CSharpOddOrEven
+{
+    public static class OddOrEvenQueue
+    {
+        public static HttpClient client = new HttpClient();
+
+        [FunctionName("OddOrEvenQueue")]
+        public static async Task RunAsync(
+            [QueueTrigger("numbers", Connection = "AzureWebJobsStorage")]string myNumber, 
+            ILogger log)
+        {
+            log.LogInformation($"Odd or even trigger fired - Queue");
+
+            if (BigInteger.TryParse(myNumber, out BigInteger number))
+            {
+                if (number % 2 == 0)
+                {
+                    log.LogInformation("Was even");
+                    await client.PostAsync("https://importantapi.com/api/transaction", new StringContent("Even"));
+                }
+                else
+                {
+                    log.LogInformation("Was odd");
+                    await client.PostAsync("https://importantapi.com/api/transaction", new StringContent("Odd"));
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"Unable to parse the queue message. Got value: {myNumber}");
+            }
+        }
+    }
+}
